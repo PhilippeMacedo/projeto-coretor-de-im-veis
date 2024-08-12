60736a65aa7b2ee6425b3a053cf95f3530c20f23
PhilippeMacedo <macedo_philippe@yahoo.com>
2024-08-12 16:55:14 -0300

    transparente menu

--- Website/script.js
+++ Website/script.js
@@ -4,10 +4,10 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "imagens/icons/arrows_hamburger_2_40392.png";
+        document.querySelector('.icon').src = "imagens/icons/white 48.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "imagens/icons/icons8-close-24.png";
+        document.querySelector('.icon').src = "imagens/icons/icons8-close-32.png";
 
     }
 
