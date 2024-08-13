fbe0acd400fa7916f44df285fa065fccf8852b37
PhilippeMacedo <macedo_philippe@yahoo.com>
2024-08-13 11:26:52 -0300

    a

--- Website/script.js
+++ Website/script.js
@@ -4,7 +4,7 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "imagens/icons/white 48.png";
+        document.querySelector('.icon').src = "imagens/icons/supostamente 24.png";
     } else {
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "imagens/icons/icons8-close-32.png";
